7067898582d0dade4813a9f6b5824724e42e62e1
OS X10_12_6 <lf@mifengs.com>
2018-09-30 15:09:19 +0800

    首次提交

---
+++ TestSwiftPod/Classes/TestView.swift
@@ -0,0 +1,23 @@
+//
+//  TestView.swift
+//  TestSwiftPod
+//
+//  Created by OS X10_12_6 on 2018/9/30.
+//  Copyright © 2018年 OS X10_12_6. All rights reserved.
+//
+
+import UIKit
+
+class TestView: UIView {
+    func testLog() {
+        print("T##items: Any...##Any")
+    }
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
